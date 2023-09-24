e37f36bdb547666ea8be1011f4e2887e5fad0b6f
JoseLeo <80642876+jose-115@users.noreply.github.com>
2023-09-24 17:06:07 -0400

    Feature/Menu Principal
    bruh

---
+++ Assets/Scripts/MenuPrincipalManager.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuPrincipalManager : MonoBehaviour
+{
+    [SerializeField] private string nomeDoLevelDeJogo;
+    [SerializeField] private GameObject painelMenuInicial;
+    [SerializeField] private GameObject painelOpcoes;   
+    public void Jogar()
+    {
+        SceneManager.LoadScene(nomeDoLevelDeJogo);
+    }
+
+    public void AbrirOpcoes()
+    {
+        painelMenuInicial.SetActive(false);
+        painelOpcoes.SetActive(true);
+    }
+
+    public void Fecharopcoes()
+    {
+        painelMenuInicial.SetActive(true);
+        painelOpcoes.SetActive(false);
+    }
+
+    public void FecharJogo()
+    {
+        Application.Quit();
+    }
+}
